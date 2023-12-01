df2f20541b7435fecc1ae67eeb6485740ac7318c
Svetlana Yatskevich <svetlana.yatskevich15@gmail.com>
2023-12-01 17:20:18 +0300

    Create 105-generate-random-fractions-with-javascript.js
    Change randomFraction to return a random number instead of returning 0.

---
+++ 105-generate-random-fractions-with-javascript.js
@@ -0,0 +1,10 @@
+function randomFraction() {
+
+  // Only change code below this line
+  let random = Math.random(0);
+  let randomFraction = random;
+  return randomFraction
+  ;
+
+  // Only change code above this line
+}
